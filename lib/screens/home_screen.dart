9e8c47f04a02183ec045a20385643d58954ea1c1
이재찬 <toxic023@naver.com>
2023-05-21 21:10:31 +0900

    bottomNavigation

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -14,28 +14,26 @@ class _HomeScreenState extends State<HomeScreen> {
   DateTime _focusedDay = DateTime.now();
   DateTime? _selectedDay;
 
+  static const String _title = "Flutter Calendar";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('TableCalendar - Basics'),
+        title: const Text(_title),
       ),
       body: TableCalendar(
         firstDay: kFirstDay,
         lastDay: kLastDay,
         focusedDay: _focusedDay,
         calendarFormat: _calendarFormat,
+        locale: 'ko-KR',
+        daysOfWeekHeight: 30,
         selectedDayPredicate: (day) {
-          // Use `selectedDayPredicate` to determine which day is currently selected.
-          // If this returns true, then `day` will be marked as selected.
-
-          // Using `isSameDay` is recommended to disregard
-          // the time-part of compared DateTime objects.
           return isSameDay(_selectedDay, day);
         },
         onDaySelected: (selectedDay, focusedDay) {
           if (!isSameDay(_selectedDay, selectedDay)) {
-            // Call `setState()` when updating the selected day
             setState(() {
               _selectedDay = selectedDay;
               _focusedDay = focusedDay;
@@ -44,16 +42,36 @@ class _HomeScreenState extends State<HomeScreen> {
         },
         onFormatChanged: (format) {
           if (_calendarFormat != format) {
-            // Call `setState()` when updating calendar format
             setState(() {
               _calendarFormat = format;
             });
           }
         },
         onPageChanged: (focusedDay) {
-          // No need to call `setState()` here
           _focusedDay = focusedDay;
         },
+        headerStyle: const HeaderStyle(
+          formatButtonVisible: false,
+          titleCentered: true,
+          leftChevronVisible: true,
+          rightChevronVisible: true,
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.abc_sharp),
+            label: 'test1',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.abc_sharp),
+            label: 'test1',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.abc_sharp),
+            label: 'test1',
+          ),
+        ],
       ),
     );
   }
