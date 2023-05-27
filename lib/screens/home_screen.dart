a98ab26bd15da01af719166e6f23ec897564507d
이재찬 <toxic023@naver.com>
2023-05-27 21:05:00 +0900

    navi

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,4 @@
 import 'package:dpj/screens/calendar.dart';
-import 'package:dpj/widgets/bottom_navi.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -11,6 +10,7 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   static const String _title = "Flutter Calendar";
+  int curIdx = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -18,8 +18,51 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: AppBar(
         title: const Text(_title),
       ),
-      body: const Calendar(),
-      bottomNavigationBar: const BottomNavi(),
+      body: Center(
+        child: bodyItem.elementAt(curIdx),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: curIdx,
+        onTap: (idx) {
+          setState(() {
+            curIdx = idx;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_outlined),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.music_note),
+            label: 'Music',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.apps),
+            label: 'Apps',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+        ],
+        //selected된 item color
+        selectedItemColor: Colors.red,
+        //unselected된 item color
+        unselectedItemColor: Colors.grey,
+        //unselected된 label text
+        showUnselectedLabels: true,
+        //BottomNavigationBar Type -> fixed = bottom item size고정
+        //BottomNavigationBar Type -> shifting = bottom item selected 된 item이 확대
+        type: BottomNavigationBarType.shifting,
+      ),
     );
   }
 }
+
+List bodyItem = [
+  const Calendar(),
+  const Text("music"),
+  const Text("apps"),
+  const Text("settings"),
+];
--- lib/widgets/bottom_navi.dart
+++ lib/widgets/bottom_navi.dart
@@ -13,25 +13,38 @@ class _BottomNaviState extends State<BottomNavi> {
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
-      onTap: (index) {
+      currentIndex: curIdx,
+      onTap: (idx) {
         setState(() {
-          curIdx = index;
+          curIdx = idx;
         });
       },
-      items: const <BottomNavigationBarItem>[
+      items: const [
         BottomNavigationBarItem(
-          icon: Icon(Icons.calendar_month_rounded),
-          label: 'Calendar',
+          icon: Icon(Icons.home_outlined),
+          label: 'Home',
         ),
         BottomNavigationBarItem(
-          icon: Icon(Icons.person),
-          label: 'Personal',
+          icon: Icon(Icons.music_note),
+          label: 'Music',
         ),
         BottomNavigationBarItem(
-          icon: Icon(Icons.account_balance),
-          label: 'Now',
+          icon: Icon(Icons.apps),
+          label: 'Apps',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.settings),
+          label: 'Settings',
         ),
       ],
+      //selected된 item color
+      selectedItemColor: Colors.red,
+      //unselected된 item color
+      unselectedItemColor: Colors.grey,
+      //unselected된 label text
+      showUnselectedLabels: true,
+      //BottomNavigationBar Type -> fixed = bottom item size고정
+      //BottomNavigationBar Type -> shifting = bottom item selected 된 item이 확대
       type: BottomNavigationBarType.shifting,
     );
   }
